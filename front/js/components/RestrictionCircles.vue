62d84780a3e91ffc8710b117da616db06f0910b7
Simon Raichl <raichls44@gmail.com>
2020-09-09 22:43:03 +0200

    restriction circles preps

--- front/js/components/RestrictionCircles.vue
+++ front/js/components/RestrictionCircles.vue
@@ -1,7 +1,40 @@
 <template>
-	
+	<section class="restriction-circles">
+		<div class="restriction-circles__item" v-for="circle in circles">
+			<img :src="circle.image" :alt="circle.name" class="m-4">
+
+			{{ circle.name }}
+		</div>
+	</section>
 </template>
 
 <script>
-	export default {}
+	export default {
+		data () {
+			return {
+				circles: [
+					{
+						name: "MHD",
+						image: "public/img/mhd.svg"
+					},
+					{
+						name: "MHD",
+						image: "public/img/mhd.svg"
+					},
+					{
+						name: "MHD",
+						image: "public/img/mhd.svg"
+					},
+					{
+						name: "MHD",
+						image: "public/img/mhd.svg"
+					},
+					{
+						name: "MHD",
+						image: "public/img/mhd.svg"
+					}
+				]
+			}
+		}
+	}
 </script>
