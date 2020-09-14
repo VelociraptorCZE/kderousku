e72bcb5378fc23c7e6ae8c1d168bbe19a998d59e
Simon Raichl <raichls44@gmail.com>
2020-09-15 01:44:34 +0200

    HomepageController refactoring

--- src/Controller/HomepageController.php
+++ src/Controller/HomepageController.php
@@ -1,8 +1,10 @@
 <?php
+declare(strict_types=1);
 
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomepageController extends AbstractController
@@ -10,9 +12,8 @@ class HomepageController extends AbstractController
     /**
      * @Route("/", name="homepage")
      */
-    public function index()
+    public function index(): Response
     {
-        return $this->render('homepage/index.html.twig', [
-        ]);
+        return $this->render('homepage/index.html.twig');
     }
 }
