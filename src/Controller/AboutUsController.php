42e6c9ce6c136507bf726f3e9c57948a90f1be6c
MartinPucan <pucan.m@seznam.cz>
2020-09-17 22:21:15 +0200

    finish about us - freepik img missing

--- src/Controller/AboutUsController.php
+++ src/Controller/AboutUsController.php
@@ -8,7 +8,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class AboutUsController extends AbstractController
 {
     /**
-     * @Route("/about/us", name="about_us")
+     * @Route("/about-us", name="about_us")
      */
     public function index()
     {
