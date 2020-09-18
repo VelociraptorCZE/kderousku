fcd9b0310755fe9707dad75ca8b3788735af5027
Simon Raichl <raichls44@gmail.com>
2020-09-18 17:25:58 +0200

    heavy refactoring

--- front/js/components/RestrictionBadges.vue
+++ front/js/components/RestrictionBadges.vue
@@ -16,50 +16,43 @@
 		</div>
 
       <article v-if="activeItem !== null">
-        <div class="card-restriction" ref="card">
-          <div class="card-restriction__left">
-            <img :src="activeItem.image" :alt="activeItem.name" class="card-restriction__right-img float-left">
+        <div class="restriction-card" ref="card">
+          <div class="restriction-card__left">
+            <img :src="activeItem.image" :alt="activeItem.name" class="restriction-card__right-img float-left">
           </div>
-          <div class="card-restriction__right">
-            <div class="card-restriction__right-header">
+          <div class="restriction-card__right">
+            <div class="restriction-card__right-header">
               {{ activeItem.name }}
             </div>
-            <div class="card-restriction__right-date">
-              <span class="card-restriction__right-range">{{ activeItem.start }} - {{ activeItem.end }}</span>
-              <span class="card-restriction__right-date-date-remain" v-if="activeItem.daysLeft">
+            <div class="restriction-card__right-date">
+              <span class="restriction-card__right-range">{{ activeItem.start }} - {{ activeItem.end }}</span>
+              <span class="restriction-card__right-date-date-remain" v-if="activeItem.daysLeft">
                 {{ activeItem.daysLeft }}
               </span>
             </div>
-            <div class="card-restriction__right-description">
-<!--              {{ activeItem.info | truncate(10, '...')}}-->
+            <div class="restriction-card__right-description">
               {{ activeItem.info }}
             </div>
           </div>
         </div>
 
 <!--         SMALL CARDS-->
-        <div class="container-cards-small">
-          <details>
-            <summary>
-              <div class="container-cards-small">
-                <div class="card-restriction-small__left">
+        <div>
+          <details class="small-restriction-card">
+            <summary class="small-restriction-card__summary">
+              <div class="d-flex p-3">
+                <div class="small-restriction-card__left">
                   <img :src="activeItem.image" :alt="activeItem.name">
                 </div>
-                <div class="card-restriction-small__right">
-                  <div class="card-restriction-small__right-header-date">
-                  <div class="summary-title">{{ activeItem.name }}</div>
-                  <div class="card-restriction-small__right-date">{{ activeItem.start }} - {{ activeItem.end }}</div>
-                  </div>
-                </div>
-                <div class="summary-chevron-up">
-                  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-down"><polyline points="6 9 12 15 18 9"></polyline></svg>
+                <div class="small-restriction-card__right">
+					<div class="small-restriction-card__right--header">{{ activeItem.name }}</div>
+					<div class="small-restriction-card__right--date">{{ activeItem.start }} - {{ activeItem.end }}</div>
                 </div>
               </div>
             </summary>
-            <div class="summary-content">{{ activeItem.info }}</div>
-            <div class="summary-chevron-down">
-              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-up"><polyline points="18 15 12 9 6 15"></polyline></svg>
-            </div>
+            <div class="small-restriction-card__content">
+				{{ activeItem.info }}
+			</div>
           </details>
         </div>
     	</article>
