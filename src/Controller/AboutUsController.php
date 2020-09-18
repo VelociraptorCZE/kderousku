e67f00ef5a75fdfe9b0f06d2f856e76302516813
Simon Raichl <raichls44@gmail.com>
2020-09-18 07:43:07 +0200

    refactoring

--- src/Controller/AboutUsController.php
+++ src/Controller/AboutUsController.php
@@ -1,8 +1,10 @@
 <?php
+declare(strict_types=1);
 
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class AboutUsController extends AbstractController
@@ -10,10 +12,8 @@ class AboutUsController extends AbstractController
     /**
      * @Route("/about-us", name="about_us")
      */
-    public function index()
+    public function index(): Response
     {
-        return $this->render('about_us/index.html.twig', [
-            'controller_name' => 'AboutUsController',
-        ]);
+        return $this->render('about_us/index.html.twig');
     }
 }
