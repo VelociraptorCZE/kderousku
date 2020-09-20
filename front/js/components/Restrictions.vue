c351ab3578794cb268d0bf02177fd38a66dcea6b
MartinPucan <pucan.m@seznam.cz>
2020-09-20 12:38:40 +0200

    refactoring img paths && add pwa

--- front/js/components/Restrictions.vue
+++ front/js/components/Restrictions.vue
@@ -43,7 +43,7 @@ export default {
 
 		if (restrictionList.current.length) {
 			restrictionList.current.unshift({
-				image: "public/img/question.svg",
+				image: "public/img/restrictions/question.svg",
 				name: "Všechna opatření",
 				showAllRestrictions: true
 			});
