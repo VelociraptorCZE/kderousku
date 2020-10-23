e84f07556662918754b49694df539f1bfe0860fd
Simon Raichl <raichls44@gmail.com>
2020-10-23 14:41:54 +0200

    region frontend fixes

--- front/js/components/RestrictionsByRegions.vue
+++ front/js/components/RestrictionsByRegions.vue
@@ -3,37 +3,34 @@
 		<div class="d-flex jcc aic p-4">
 			<h2>Kraj</h2>
 
-      <div>
-        <label>
-          <select @input="activeRegion = $event.target.value" class="m-3 select">
-            <option :value="regionName" v-for="regionName in Object.keys(restrictionList)">
-              {{ regionName }}
-            </option>
-          </select>
-          <span class="focus"></span>
-        </label>
-      </div>
-    </div>
+			<div>
+				<label>
+					<select @input="activeRegion = $event.target.value" class="m-3 select">
+						<option :value="regionName" v-for="regionName in Object.keys(restrictionList)">
+							{{ regionName }}
+						</option>
+					</select>
+					<span class="focus"></span>
+				</label>
+			</div>
+		</div>
 
 		<article v-if="restrictionList[activeRegion]">
 			<div v-if="!restrictionList[activeRegion].length">
 				<h3 class="tc">V tomto kraji nevíme o žádném nařízení</h3>
 			</div>
 
-			<article class="container-regions p-4" v-for="restriction in restrictionList[activeRegion]" v-else>
-				<div>
-          <h3 class="mt-0 tc h3">Stredocesky</h3>
-        </div>
-        <div class="container-regions__cards">
-          <div class="region-card--body">
-            <div class="region-card--title">
-              {{ restriction.name }}
-            </div>
-            <div class="region-card--content">
-              {{ restriction.info }}
-            </div>
-          </div>
-        </div>
+			<article class="container-regions p-4" v-else>
+				<div class="container-regions__cards">
+					<div class="region-card--body" v-for="restriction in restrictionList[activeRegion]">
+						<div class="region-card--title">
+							{{ restriction.name }}
+						</div>
+						<div class="region-card--content">
+							{{ restriction.info }}
+						</div>
+					</div>
+				</div>
 			</article>
 		</article>
 	</section>
